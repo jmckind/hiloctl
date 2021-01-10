eaf2d695c083997cfed8681069d17e17306086ec
John McKenzie <jmckind@gmail.com>
2021-01-10 15:51:38 -0600

    Clean up comments

--- cmd/root.go
+++ cmd/root.go
@@ -42,18 +42,10 @@ func Execute() {
 	}
 }
 
+// init will initialize the root command.
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.hiloctl.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
